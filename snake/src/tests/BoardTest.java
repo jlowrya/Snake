1ec79f97c667bd05bfabb8035ea826c6521718d7
jlowrya <lowry2234@gmail.com>
2018-12-29 23:20:12 -0500

    App creation
    Created Snake, Board, and associated test classes.

---
+++ snake/src/tests/BoardTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import production.Board;
+
+public class BoardTest {
+    Board board;
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testInitialization(){
+
+    }
+
+    @Test
+    public void testGetToken(){
+
+    }
+
+    @Test
+    public void testSetToken(){
+
+    }
+
+}
