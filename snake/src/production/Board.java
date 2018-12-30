a84fa96bb8d9607a24a44080b907a6d204fbd709
james <jlowrya@essex.ac.uk>
2018-12-29 23:36:31 -0500

    Created constructor method for Board as well as getter method for board and token, and a setter for token.

--- snake/src/production/Board.java
+++ snake/src/production/Board.java
@@ -1,4 +1,25 @@
 package production;
 
 public class Board {
+    private char[][] board;
+
+    private char token;
+
+    public Board(){
+        board = new char[10][10];
+        token = 't';
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public char getToken() {
+        return token;
+    }
+
+    public void setToken(char token) {
+        this.token = token;
+    }
+
 }
--- snake/src/tests/BoardTest.java
+++ snake/src/tests/BoardTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import production.Board;
@@ -9,22 +10,23 @@ public class BoardTest {
 
     @Before
     public void setup(){
-
+        board = new Board();
     }
 
     @Test
     public void testInitialization(){
-
+        Assert.assertTrue(board.getBoard().length == 10 && board.getBoard()[0].length==10);
     }
 
     @Test
     public void testGetToken(){
-
+        Assert.assertTrue(board.getToken()=='t');
     }
 
     @Test
     public void testSetToken(){
-
+        board.setToken('s');
+        Assert.assertTrue(board.getToken()=='s');
     }
 
 }
